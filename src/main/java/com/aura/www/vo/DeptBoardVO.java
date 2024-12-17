5990235adf1245f93fc85bd1b1c2e7afd2d25183
king <king@localhost>
2024-12-17 23:42:16 +0900

    AdminDeptDAO, DeptVO 추가 -- 중간 commit

--- src/main/java/com/aura/www/vo/DeptBoardVO.java
+++ src/main/java/com/aura/www/vo/DeptBoardVO.java
@@ -5,26 +5,6 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-//	SQL> CREATE TABLE BOARD
-//	2  (BNO NUMBER(5),
-//	3  WRITER VARCHAR2(20),
-//	4  TITLE VARCHAR2(50),
-//	5  CONTENTS CLOB,
-//	6  REGDATE DATE,
-//	7  HITS NUMBER(5),
-//	8  IP VARCHAR2(16),
-//	9  STATUS NUMBER(2));
-//
-//	테이블이 생성되었습니다.
-//
-//	SQL> CREATE SEQUENCE BOARD_BNO_SEQ
-//	2  START WITH 1
-//	3  INCREMENT BY 1
-//	4  NOCACHE
-//	5  NOCYCLE;
-//
-//	시퀀스가 생성되었습니다.
-
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
---
+++ src/main/java/com/aura/www/vo/DeptVO.java
@@ -0,0 +1,15 @@
+package com.aura.www.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class DeptVO {
+	int deptno;
+	String dname;
+	String loc;
+	
+}
