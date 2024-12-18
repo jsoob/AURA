d8640b5b4ef0f2b2e4f6ba1810c38b6c596f64bc
JS <kmhe0128@naver.com>
2024-12-18 16:37:57 +0900

    login main jsp 파일과 MainAction 변경

--- src/main/java/com/aura/www/action/main/MainAction.java
+++ src/main/java/com/aura/www/action/main/MainAction.java
@@ -7,27 +7,27 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 public class MainAction implements Action {
+	
+	@Override
+	public String execute(HttpServletRequest req, HttpServletResponse resp) {
+		String url = "";
+		
+		HashMap<String, String> map = new HashMap<String, String>();
 
-@Override
-public String execute(HttpServletRequest req, HttpServletResponse resp) {
-String url = "";
-
-HashMap<String, String> map = new HashMap<String, String>();
-
-map.put("title", "AURA"); // 웹 제목?
-map.put("category", "main"); // 카테고리 찾는 key
-map.put("categoryName", "Main"); // 사용자에게 보여주는 카테고리명
-map.put("pages", "main"); // 페이지명
-map.put("pagesName", "메인 화면"); // 사용자에게 보여주는 페이지명
-
-req.setAttribute("commAt", map);
-
-//req.setAttribute("title", "AURA");
-//req.setAttribute("catecory", "main");
-//req.setAttribute("pages", "main");
-
-url = "view/main/main.jsp";
-return url;
-}
-
+		map.put("title", "AURA"); // 웹 제목?
+		map.put("category", "main"); // 카테고리 찾는 key
+		map.put("categoryName", "Main"); // 사용자에게 보여주는 카테고리명
+		map.put("pages", "main"); // 페이지명
+		map.put("pagesName", "메인 화면"); // 사용자에게 보여주는 페이지명
+			
+		req.setAttribute("commAt", map);
+		
+//		req.setAttribute("title", "AURA");
+//		req.setAttribute("catecory", "main");
+//		req.setAttribute("pages", "main");
+		
+		url = "view/main/main.jsp";
+		return url;
+	}
+	
 }
