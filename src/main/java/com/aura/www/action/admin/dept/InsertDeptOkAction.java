250105c87d39030219860ec4e8a55db3e3b0869a
king <king@localhost>
2024-12-20 10:10:37 +0900

    admin - dept commit

--- src/main/java/com/aura/www/action/admin/dept/InsertDeptOkAction.java
+++ src/main/java/com/aura/www/action/admin/dept/InsertDeptOkAction.java
@@ -1,17 +1,34 @@
 package com.aura.www.action.admin.dept;
 
 import com.aura.www.action.Action;
+import com.aura.www.dao.AdminDeptDAO;
+import com.aura.www.vo.DeptVO;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-public class InsertDeptOkAction implements Action{
+public class InsertDeptOkAction implements Action {
 
 	@Override
 	public String execute(HttpServletRequest req, HttpServletResponse resp) {
-	
-		
-		return null;
+
+		// 파라미터값 가져와서 db에 저장
+		String deptNo = req.getParameter("deptNo");
+		String deptName = req.getParameter("deptName");
+
+		if (deptNo != null) {
+			int deptno = Integer.parseInt(deptNo);
+			AdminDeptDAO dao = new AdminDeptDAO();
+
+			DeptVO vo = new DeptVO();
+
+			vo.setDeptNo(deptno);
+			vo.setDeptName(deptName);
+
+			dao.insertDept(vo);
+		}
+
+		return "admin?cmd=selectDept";
 	}
 
 }
--- src/main/java/com/aura/www/action/admin/dept/SelectDeptAction.java
+++ src/main/java/com/aura/www/action/admin/dept/SelectDeptAction.java
@@ -1,22 +1,35 @@
 package com.aura.www.action.admin.dept;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import com.aura.www.action.Action;
-import com.aura.www.dao.PositionDAO;
-import com.aura.www.vo.PositionVO;
+import com.aura.www.dao.AdminDeptDAO;
+import com.aura.www.vo.DeptVO;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-public class SelectDeptAction implements Action{
+public class SelectDeptAction implements Action {
 
 	@Override
 	public String execute(HttpServletRequest req, HttpServletResponse resp) {
-		
-		
-		
-		return null;
+		HashMap<String, String> map = new HashMap<String, String>();
+
+		map.put("title", "AURA 부서관리 페이지"); // 웹 제목?
+		map.put("category", "dept"); // 카테고리 찾는 key
+		map.put("categoryName", "부서 관리 페이지"); // 사용자에게 보여주는 카테고리명
+		map.put("pages", "selectDept"); // 페이지명
+		map.put("pagesName", "부서 관리"); // 사용자에게 보여주는 페이지명
+
+		req.setAttribute("commAt", map);
+
+		AdminDeptDAO dao = new AdminDeptDAO();
+		ArrayList<DeptVO> list = dao.selectDeptAll();
+
+		req.setAttribute("list", list);
+
+		return "view/admin/selectDept.jsp";
 	}
 
 }
